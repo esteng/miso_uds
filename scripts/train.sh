857bb1d331733c6b90efe88bd69b435cf1996e7e
Sheng Zhang <zhangsheng.zero@gmail.com>
2018-10-03 22:18:21 -0400

    Change the way of using train.sh.

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 source activate stog
-stog=".."
-python ${stog}/train.py \
-  --train_data ${stog}/data/json/train.json \
-  --dev_data ${stog}/data/json/dev.json \
+
+python -u -m train  \
+  --train_data ./data/json/dev.json \
+  --dev_data ./data/json/dev.json \
   --token_emb_size 50 \
   --batch_first \
   --shuffle \
   --save_model model \
+  --gpu
