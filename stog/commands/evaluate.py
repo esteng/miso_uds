97e15f39908090d15da34d8284a4d365509f6bd7
Sheng Zhang <zhangsheng.zero@gmail.com>
2018-10-13 13:01:58 -0400

    Bugfix.

--- stog/commands/evaluate.py
+++ stog/commands/evaluate.py
@@ -15,7 +15,7 @@ def evaluate(model: Model,
              instances,
              Iterator,
              batch_size,
-             cuda_device: int) -> Dict[str, Any]:
+             cuda_device: int):
     environment.check_for_gpu(cuda_device)
 
     with torch.no_grad():
@@ -47,18 +47,20 @@ def add_predictions(output_dict, fields, predictions):
     def _interpret(data, lengths, vocab=None):
         instances = []
         assert data.size(0) == lengths.size(0)
+        lengths = lengths.long().tolist()
         for instance_ids, length in zip(data, lengths):
             if vocab:
-                instance = [vocab.itos(i.item()) for i in instance_ids[:length]]
+                instance = [vocab.itos[i] for i in instance_ids[:length].tolist()]
             else:
-                instance = instance_ids.tolist()
+                instance = instance_ids[:length].tolist()
             instances.append(instance)
         return instances
 
     output_dict.pop('loss', None)
     lengths = output_dict.pop('mask').detach().cpu().sum(dim=1)
     for name, pred in output_dict.items():
-        if name in fields:
+        pred = pred.detach().cpu()
+        if name in fields and hasattr(fields[name], 'vocab'):
             instances = _interpret(pred, lengths, fields[name].vocab)
         else:
             instances = _interpret(pred, lengths)
