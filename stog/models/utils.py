a6b396e3a2eb756dfeb5833e8442425b351dacec
Xutai Ma <xutai_ma@outlook.com>
2018-10-15 15:12:36 -0400

    add model util

--- stog/models/utils.py
+++ stog/models/utils.py
@@ -48,3 +48,37 @@ def get_text_field_mask(text_field_tensors: Dict[str, torch.Tensor],
         return ((character_tensor > 0).long().sum(dim=-1) > 0).long()
     else:
         raise ValueError("Expected a tensor with dimension 2 or 3, found {}".format(smallest_dim))
+
+
+def has_tensor(obj) -> bool:
+    """
+    Given a possibly complex data structure,
+    check if it has any torch.Tensors in it.
+    """
+    if isinstance(obj, torch.Tensor):
+        return True
+    elif isinstance(obj, dict):
+        return any(has_tensor(value) for value in obj.values())
+    elif isinstance(obj, (list, tuple)):
+        return any(has_tensor(item) for item in obj)
+    else:
+        return False
+
+
+def move_to_device(obj, cuda_device: int):
+    """
+    Given a structure (possibly) containing Tensors on the CPU,
+    move all the Tensors to the specified GPU (or do nothing, if they should be on the CPU).
+    """
+    if cuda_device < 0 or not has_tensor(obj):
+        return obj
+    elif isinstance(obj, torch.Tensor):
+        return obj.cuda(cuda_device)
+    elif isinstance(obj, dict):
+        return {key: move_to_device(value, cuda_device) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [move_to_device(item, cuda_device) for item in obj]
+    elif isinstance(obj, tuple):
+        return tuple([move_to_device(item, cuda_device) for item in obj])
+    else:
+        return obj
