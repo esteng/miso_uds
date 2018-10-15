ec89cc7fbdce512c00bbe43f93d529057f6d3e90
Xutai Ma <xutai_ma@jhu.edu>
2018-10-15 14:23:41 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version='0.1',
    description='A semantics parsing framework',
    packages=['stog'],  #same as name
-   install_requires=['overrides', 'torch==0.4.1', 'h5py', 'boto3', 'tensorboardX==1.2']
+   install_requires=['overrides', 'torch==0.4.1', 'h5py', 'boto3', 'tensorboardX==1.2','spacy', 'ftfy', 'nltk', 'conllu']
 )
 
 # Write logs for training visualisation with the Tensorboard application
