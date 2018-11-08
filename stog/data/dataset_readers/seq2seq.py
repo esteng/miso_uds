c6961460036e50a13aa4c6b36830fa16b17b0b3f
Xutai Ma <xutai_ma@jhu.edu>
2018-11-08 10:56:00 -0500

    add mt task

--- stog/data/dataset_readers/seq2seq.py
+++ stog/data/dataset_readers/seq2seq.py
@@ -47,17 +47,11 @@ class Seq2SeqDatasetReader(DatasetReader):
         Whether or not to add `START_SYMBOL` to the beginning of the source sequence.
     """
     def __init__(self,
-                 source_tokenizer: Tokenizer = None,
-                 target_tokenizer: Tokenizer = None,
-                 source_token_indexers: Dict[str, TokenIndexer] = None,
-                 target_token_indexers: Dict[str, TokenIndexer] = None,
-                 source_add_start_token: bool = True,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 source_add_start_token : bool = False,
                  lazy: bool = False) -> None:
         super().__init__(lazy)
-        self._source_tokenizer = source_tokenizer or WordTokenizer()
-        self._target_tokenizer = target_tokenizer or self._source_tokenizer
-        self._source_token_indexers = source_token_indexers or {"tokens": SingleIdTokenIndexer()}
-        self._target_token_indexers = target_token_indexers or self._source_token_indexers
+        self._token_indexers = token_indexers
         self._source_add_start_token = source_add_start_token
 
     @overrides
@@ -79,16 +73,29 @@ class Seq2SeqDatasetReader(DatasetReader):
     @overrides
     def text_to_instance(self, source_string: str, target_string: str = None) -> Instance:  # type: ignore
         # pylint: disable=arguments-differ
-        tokenized_source = self._source_tokenizer.tokenize(source_string)
+        #tokenized_source = self._source_tokenizer.tokenize(source_string)
+        tokenized_source = [Token(x) for x in source_string.split(' ')]
         if self._source_add_start_token:
             tokenized_source.insert(0, Token(START_SYMBOL))
         tokenized_source.append(Token(END_SYMBOL))
-        source_field = TextField(tokenized_source, self._source_token_indexers)
-        if target_string is not None:
-            tokenized_target = self._target_tokenizer.tokenize(target_string)
-            tokenized_target.insert(0, Token(START_SYMBOL))
-            tokenized_target.append(Token(END_SYMBOL))
-            target_field = TextField(tokenized_target, self._target_token_indexers)
-            return Instance({"source_tokens": source_field, "target_tokens": target_field})
-        else:
-            return Instance({'source_tokens': source_field})
+        
+        
+
+        tokenized_target = [Token(x) for x in target_string.split(" ")]
+        tokenized_target.insert(0, Token(START_SYMBOL))
+        tokenized_target.append(Token(END_SYMBOL))
+        
+        source_field = TextField(
+            tokenized_source,
+            token_indexers={k: v for k, v in self._token_indexers.items() if 'encoder' in k}
+        )
+        target_field = TextField(
+            tokenized_target,
+            token_indexers={k: v for k, v in self._token_indexers.items() if 'decoder' in k}
+        )
+
+        fields: Dict[str, Field] = {}
+        fields["src_tokens"] = source_field
+        fields["amr_tokens"] = target_field
+        
+        return Instance(fields)
