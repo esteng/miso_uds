cce942b75ee23d3db4fdbe620a051ec90ca84993
Sheng Zhang <zhangsheng.zero@gmail.com>
2018-12-07 10:43:44 -0500

    Keep quotes in preprocessing.

--- stog/data/dataset_readers/amr_parsing/preprocess/aligner.py
+++ stog/data/dataset_readers/amr_parsing/preprocess/aligner.py
@@ -26,10 +26,10 @@ class Aligner:
         # TODO: add more rules.
         # Get AMR lemma.
         if isinstance(value, str):
-            if re.search(r'-\d\d$', value):  # frame
+            if re.search(r'^".*"$', value):  # literal constant
+                amr_lemma = value
+            elif re.search(r'-\d\d$', value):  # frame
                 amr_lemma = self.node_utils.get_lemma(value)
-            elif re.search(r'^".*"$', value):  # literal constant
-                amr_lemma = value[1:-1]
             else:
                 amr_lemma = value
         else:
@@ -37,22 +37,21 @@ class Aligner:
             amr_lemma = value
         return amr_lemma
 
-    @staticmethod
-    def align_amr_lemma(amr_lemma, amr):
+    def align_amr_lemma(self, amr_lemma, amr):
         # TODO: Add more align rules.
         # amr_lemma is case-sensitive, so try casing it in different ways: Aaa, AAA, aaa.
+        self.amr_lemma_count += 1
         if str(amr_lemma) in amr.lemmas:
-            return amr_lemma
-        return None
+            self.align_count += 1
+        elif re.search(r'^".*"$', str(amr_lemma)) and amr_lemma[1:-1] in amr.lemmas:  # literal constant
+            self.align_count += 1
+        else:
+            self.no_align_lemma_set.add(amr_lemma)
+        return amr_lemma
 
     def update_graph(self, graph, node, attr, old, new):
-        self.amr_lemma_count += 1
-        if new is not None:
-            self.align_count += 1
-            graph.replace_node_attribute(node, attr, old, new)
-            self.try_restore(old, new)
-        else:
-            self.no_align_lemma_set.add(old)
+        graph.replace_node_attribute(node, attr, old, new)
+        self.try_restore(old, new)
 
     def try_restore(self, old, new):
         if not isinstance(old, str):
@@ -60,8 +59,6 @@ class Aligner:
                 _old = int(new)
             else:
                 _old = float(new)
-        elif re.search(r'^".*"$', old):  # literal constant
-            _old = '"' + new + '"'
         else:
             _old = self.node_utils.get_frame(new)
         self.restore_count += int(old == _old)
@@ -69,14 +66,14 @@ class Aligner:
     def reset_statistics(self):
         self.align_count = 0
         self.amr_lemma_count = 0
-        self.no_align_lemma_set = set()
         self.restore_count = 0
+        self.no_align_lemma_set = set()
 
     def print_statistics(self):
         print('align rate: {}% ({}/{})'.format(
             self.align_count / self.amr_lemma_count, self.align_count, self.amr_lemma_count))
         print('restore rate: {}% ({}/{})'.format(
-            self.restore_count / self.align_count, self.restore_count, self.align_count))
+            self.restore_count / self.amr_lemma_count, self.restore_count, self.amr_lemma_count))
         print('size of no align lemma set: {}'.format(len(self.no_align_lemma_set)))
 
 
@@ -86,24 +83,24 @@ if __name__ == '__main__':
     from stog.data.dataset_readers.amr_parsing.node_utils import NodeUtilities as NU
 
     parser = argparse.ArgumentParser('aligner.py')
-    parser.add_argument('--amr_train_files', nargs='+', required=True)
+    parser.add_argument('--amr_train_files', nargs='+')
     parser.add_argument('--amr_dev_files', nargs='+', required=True)
-    parser.add_argument('--propbank_dir', default='data/misc/propbank-frames-xml-2016-03-08/')
-    parser.add_argument('--verbalize_file', default='data/misc/verbalization-list-v1.06.txt')
+    parser.add_argument('--json_dir', default='./temp')
 
     args = parser.parse_args()
 
-    node_utils = NU(args.amr_train_files, args.propbank_dir, args.verbalize_file, args.amr_dev_files)
+    node_utils = NU.from_json(args.json_dir)
 
     aligner = Aligner(node_utils)
 
-    for file_path in args.amr_train_files:
-        with open(file_path + '.align', 'w', encoding='utf-8') as f:
-            for amr in aligner.align_file(file_path):
-                f.write(str(amr) + '\n\n')
+    if len(args.amr_train_files) != 0:
+        for file_path in args.amr_train_files:
+            with open(file_path + '.align', 'w', encoding='utf-8') as f:
+                for amr in aligner.align_file(file_path):
+                    f.write(str(amr) + '\n\n')
 
-    aligner.print_statistics()
-    aligner.reset_statistics()
+        aligner.print_statistics()
+        aligner.reset_statistics()
 
     for file_path in args.amr_dev_files:
         with open(file_path + '.align', 'w', encoding='utf-8') as f:
