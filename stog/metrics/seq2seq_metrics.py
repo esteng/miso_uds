a7208994bb7ae8f41ecfebba196dda9c1f112b5f
Sheng Zhang <zhangsheng.zero@gmail.com>
2018-12-20 21:04:17 -0500

    Update seq2seq_metrics.py.

--- stog/metrics/seq2seq_metrics.py
+++ stog/metrics/seq2seq_metrics.py
@@ -15,24 +15,38 @@ class Seq2SeqMetrics(Metric):
     * elapsed time
     """
 
-    def __init__(self, loss=0, n_words=0, n_correct=0, n_copies=0, n_correct_copies=0, n_correct_binaries=0):
+    def __init__(self, loss=0, n_words=0, n_correct=0,
+                 n_source_copies=0, n_correct_source_copies=0, n_correct_source_points=0,
+                 n_target_copies=0, n_correct_target_copies=0, n_correct_target_points=0
+                 ):
         self.loss = loss
         self.n_words = n_words
         self.n_correct = n_correct
-        self.n_copies = n_copies
-        self.n_correct_copies = n_correct_copies
-        self.n_correct_binaries = n_correct_binaries
 
-    def __call__(self, loss, n_words, n_correct, n_copies=0, n_correct_copies=0, n_correct_binaries=0):
+        self.n_source_copies = n_source_copies
+        self.n_correct_source_copies = n_correct_source_copies
+        self.n_correct_source_points = n_correct_source_points
+
+        self.n_target_copies = n_target_copies
+        self.n_correct_target_copies = n_correct_target_copies
+        self.n_correct_target_points = n_correct_target_points
+
+    def __call__(self, loss, n_words, n_correct,
+                 n_source_copies=0, n_correct_source_copies=0, n_correct_source_points=0,
+                 n_target_copies=0, n_correct_target_copies=0, n_correct_target_points=0
+                 ):
         """
         Update statistics by suming values with another `Statistics` object
         """
         self.loss += loss
         self.n_words += n_words
         self.n_correct += n_correct
-        self.n_copies += n_copies
-        self.n_correct_copies += n_correct_copies
-        self.n_correct_binaries += n_correct_binaries
+        self.n_source_copies += n_source_copies
+        self.n_correct_source_copies += n_correct_source_copies
+        self.n_correct_source_points += n_correct_source_points
+        self.n_target_copies += n_target_copies
+        self.n_correct_target_copies += n_correct_target_copies
+        self.n_correct_target_points += n_correct_target_points
 
     def accuracy(self):
         """ compute accuracy """
@@ -46,24 +60,19 @@ class Seq2SeqMetrics(Metric):
         """ compute perplexity """
         return math.exp(min(self.loss / self.n_words, 100))
 
-    def copy_accuracy(self):
-        if self.n_copies == 0:
+    def copy_accuracy(self, n_correct, n_copies):
+        if n_copies == 0:
             return -1
         else:
-            return 100 * (self.n_correct_copies / self.n_copies)
-
-    def binary_accuracy(self):
-        if self.n_copies == 0:
-            return -1
-        else:
-            return 100 * (self.n_correct_binaries / self.n_copies)
+            return 100 * (n_correct / n_copies)
 
     def get_metric(self, reset: bool = False):
         metrics = dict(
-            accuracy=self.accuracy(),
-            copy_acc=self.copy_accuracy(),
-            bina_acc=self.binary_accuracy(),
-            xent=self.xent(),
+            all_acc=self.accuracy(),
+            src_acc=self.copy_accuracy(self.n_correct_source_copies, self.n_source_copies),
+            tgt_acc=self.copy_accuracy(self.n_correct_target_copies, self.n_target_copies),
+            # bina_acc=self.binary_accuracy(),
+            # xent=self.xent(),
             ppl=self.ppl()
         )
         if reset:
@@ -75,6 +84,9 @@ class Seq2SeqMetrics(Metric):
         self.loss = 0
         self.n_words = 0
         self.n_correct = 0
-        self.n_copies = 0
-        self.n_correct_copies = 0
-        self.n_correct_binaries = 0
+        self.n_target_copies = 0
+        self.n_correct_target_copies= 0
+        self.n_correct_target_points = 0
+        self.n_source_copies = 0
+        self.n_correct_source_copies = 0
+        self.n_correct_source_points = 0
