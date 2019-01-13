7e55485caf6055184b255edba9edbe27c45537ba
Sheng Zhang <zhangsheng.zero@gmail.com>
2019-01-13 11:15:32 -0500

    Share linear in coref attn.

--- stog/modules/attention/dot_production_attention.py
+++ stog/modules/attention/dot_production_attention.py
@@ -3,14 +3,12 @@ import torch
 
 class DotProductAttention(torch.nn.Module):
 
-    def __init__(self, decoder_hidden_size, encoder_hidden_size, add_linear=True):
+    def __init__(self, decoder_hidden_size, encoder_hidden_size, share_linear=True):
         super(DotProductAttention, self).__init__()
         self.decoder_hidden_size = decoder_hidden_size
         self.encoder_hidden_size = encoder_hidden_size
-        if add_linear:
-            self.linear_layer = torch.nn.Linear(decoder_hidden_size, encoder_hidden_size, bias=False)
-        else:
-            self.linear_layer = None
+        self.linear_layer = torch.nn.Linear(decoder_hidden_size, encoder_hidden_size, bias=False)
+        self.share_linear = share_linear
 
     def forward(self, decoder_input, encoder_input):
         """
@@ -18,8 +16,9 @@ class DotProductAttention(torch.nn.Module):
         :param encoder_input:  [batch, encoder_seq_length, encoder_hidden_size]
         :return:  [batch, decoder_seq_length, encoder_seq_length]
         """
-        if self.linear_layer is not None:
-            decoder_input = self.linear_layer(decoder_input)
+        decoder_input = self.linear_layer(decoder_input)
+        if self.share_linear:
+            encoder_input = self.linear_layer(encoder_input)
 
         encoder_input = encoder_input.transpose(1, 2)
         return torch.bmm(decoder_input, encoder_input)
