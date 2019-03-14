df761070c24ca3a0b078a194a21418bbfef0c45b
Sheng Zhang <zhangsheng.zero@gmail.com>
2019-03-14 10:24:23 -0400

    Start building iStog.

--- miso/models/__init__.py
+++ miso/models/__init__.py
@@ -1,5 +1,6 @@
 from .deep_biaffine_parser import DeepBiaffineParser
 from .seq2seq import Seq2Seq
 from .stog import STOG
+from .istog import ISTOG
 from .model import Model
 from .language_model import LanguageModel
---
+++ miso/models/istog/__init__.py
@@ -0,0 +1,1 @@
+from .istog import ISTOG
