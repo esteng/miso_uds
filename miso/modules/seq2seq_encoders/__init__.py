3dd7036b8e23afca2dcdeb6f3d802ec6db337d6d
Sheng Zhang <zhangsheng.zero@gmail.com>
2020-02-10 16:00:34 -0500

    Update seq2seq encoders.

--- miso/modules/seq2seq_encoders/__init__.py
+++ miso/modules/seq2seq_encoders/__init__.py
@@ -1,2 +1,44 @@
-from miso.modules.seq2seq_encoders.pytorch_seq2seq_wrapper import PytorchSeq2SeqWrapper
-from miso.modules.seq2seq_encoders.seq2seq_bert_encoder import Seq2SeqBertEncoder
+from allennlp.common import Params
+from allennlp.common.checks import ConfigurationError
+from allennlp.modules.seq2seq_encoders import PytorchSeq2SeqWrapper, Seq2SeqEncoder
+from allennlp.modules.seq2seq_encoders import _Seq2SeqWrapper
+from allennlp.modules.encoder_base import RnnStateStorage
+from allennlp.modules.stacked_bidirectional_lstm import StackedBidirectionalLstm
+
+
+class _PytorchSeq2SeqWrapper(PytorchSeq2SeqWrapper):
+    """
+    Inherit ``PytorchSeq2SeqWrapper'' to expose the final states.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def get_final_states(self) -> RnnStateStorage:
+        return self._states
+
+
+class _MisoSeq2SeqWrapper(_Seq2SeqWrapper):
+    """
+    It inherits ``_Seq2SeqWrapper'' from AllenNLP such that we have more flexibility
+    of defining our own seq2seq encoders.
+    """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, **kwargs) -> PytorchSeq2SeqWrapper:
+        return self.from_params(Params(kwargs))
+
+    # Logic requires custom from_params
+    def from_params(self, params: Params) -> _PytorchSeq2SeqWrapper:
+        if not params.pop_bool('batch_first', True):
+            raise ConfigurationError("Our encoder semantics assumes batch is always first!")
+        if self._module_class in self.PYTORCH_MODELS:
+            params['batch_first'] = True
+        stateful = params.pop_bool('stateful', False)
+        module = self._module_class(**params.as_dict(infer_type_and_cast=True))
+        return _PytorchSeq2SeqWrapper(module, stateful=stateful)
+
+
+# pylint: disable=protected-access
+Seq2SeqEncoder.register("miso_stacked_bilstm")(_MisoSeq2SeqWrapper(StackedBidirectionalLstm))
--- miso/modules/seq2seq_encoders/seq2seq_bert_encoder.py
+++ miso/modules/seq2seq_encoders/seq2seq_bert_encoder.py
@@ -1,74 +1,88 @@
 import torch
 
-from pytorch_pretrained_bert.modeling import BertModel
+from allennlp.common import Registrable
+from pytorch_transformers import BertModel
 
 
-class Seq2SeqBertEncoder(BertModel):
+class BaseBertWrapper(torch.nn.Module, Registrable):
 
-    def __init__(self, config):
-        super(Seq2SeqBertEncoder, self).__init__(config)
+    def __init__(self, config: str) -> None:
+        super().__init__()
+        self.bert_model = BertModel(config)
 
-    def forward(self, input_ids,
-                token_type_ids=None,
-                attention_mask=None,
-                output_all_encoded_layers=True,
-                token_subword_index=None):
+
+@BaseBertWrapper.register("seq2seq_bert_encoder")
+class Seq2SeqBertEncoder(BaseBertWrapper):
+
+    def __init__(self, config: str) -> None:
+        super().__init__(config)
+
+    def forward(self,
+                input_ids: torch.LongTensor,
+                token_type_ids: torch.Tensor = None,
+                attention_mask: torch.Tensor = None,
+                output_all_encoded_layers: bool = True,
+                token_subword_index: torch.LongTensor = None) -> torch.Tensor:
         """
         :param input_ids: same as it in BertModel
         :param token_type_ids: same as it in BertModel
         :param attention_mask: same as it in BertModel
         :param output_all_encoded_layers: same as it in BertModel
         :param token_subword_index: [batch_size, num_tokens, num_subwords]
-        :return:
         """
         # encoded_layers: [batch_size, num_subword_pieces, hidden_size]
-        encoded_layers, pooled_output = super(Seq2SeqBertEncoder, self).forward(
+        encoded_layers, _ = self.bert_model(
             input_ids, token_type_ids, attention_mask, output_all_encoded_layers)
         if token_subword_index is None:
-            return encoded_layers, pooled_output
+            return encoded_layers
         else:
-            return self.average_pooling(encoded_layers, token_subword_index), pooled_output
+            return average_pooling(encoded_layers, token_subword_index)
 
-    def average_pooling(self, encoded_layers, token_subword_index):
-        batch_size, num_tokens, num_subwords = token_subword_index.size()
-        batch_index = torch.arange(batch_size).view(-1, 1, 1).type_as(token_subword_index)
-        token_index = torch.arange(num_tokens).view(1, -1, 1).type_as(token_subword_index)
-        _, num_total_subwords, hidden_size = encoded_layers.size()
-        expanded_encoded_layers = encoded_layers.unsqueeze(1).expand(
-            batch_size, num_tokens, num_total_subwords, hidden_size)
-        # [batch_size, num_tokens, num_subwords, hidden_size]
-        token_reprs = expanded_encoded_layers[batch_index, token_index, token_subword_index]
-        subword_pad_mask = token_subword_index.eq(0).unsqueeze(3).expand(
-            batch_size, num_tokens, num_subwords, hidden_size)
-        token_reprs.masked_fill_(subword_pad_mask, 0)
-        # [batch_size, num_tokens, hidden_size]
-        sum_token_reprs = torch.sum(token_reprs, dim=2)
-        # [batch_size, num_tokens]
-        num_valid_subwords = token_subword_index.ne(0).sum(dim=2)
-        pad_mask = num_valid_subwords.eq(0).long()
-        # Add ones to arrays where there is no valid subword.
-        divisor = (num_valid_subwords + pad_mask).unsqueeze(2).type_as(sum_token_reprs)
-        # [batch_size, num_tokens, hidden_size]
-        avg_token_reprs = sum_token_reprs / divisor
-        return avg_token_reprs
 
-    def max_pooling(self, encoded_layers, token_subword_index):
-        batch_size, num_tokens, num_subwords = token_subword_index.size()
-        batch_index = torch.arange(batch_size).view(-1, 1, 1).type_as(token_subword_index)
-        token_index = torch.arange(num_tokens).view(1, -1, 1).type_as(token_subword_index)
-        _, num_total_subwords, hidden_size = encoded_layers.size()
-        expanded_encoded_layers = encoded_layers.unsqueeze(1).expand(
-            batch_size, num_tokens, num_total_subwords, hidden_size)
-        # [batch_size, num_tokens, num_subwords, hidden_size]
-        token_reprs = expanded_encoded_layers[batch_index, token_index, token_subword_index]
-        subword_pad_mask = token_subword_index.eq(0).unsqueeze(3).expand(
-            batch_size, num_tokens, num_subwords, hidden_size)
-        token_reprs.masked_fill_(subword_pad_mask, -float('inf'))
-        # [batch_size, num_tokens, hidden_size]
-        max_token_reprs, _ = torch.max(token_reprs, dim=2)
-        # [batch_size, num_tokens]
-        num_valid_subwords = token_subword_index.ne(0).sum(dim=2)
-        pad_mask = num_valid_subwords.eq(0).unsqueeze(2).expand(
-            batch_size, num_tokens, hidden_size)
-        max_token_reprs.masked_fill(pad_mask, 0)
-        return max_token_reprs
+def average_pooling(encoded_layers: torch.FloatTensor,
+                    token_subword_index: torch.LongTensor) -> torch.Tensor:
+    batch_size, num_tokens, num_subwords = token_subword_index.size()
+    batch_index = torch.arange(batch_size).view(-1, 1, 1).type_as(token_subword_index)
+    token_index = torch.arange(num_tokens).view(1, -1, 1).type_as(token_subword_index)
+    _, num_total_subwords, hidden_size = encoded_layers.size()
+    expanded_encoded_layers = encoded_layers.unsqueeze(1).expand(
+        batch_size, num_tokens, num_total_subwords, hidden_size)
+    # [batch_size, num_tokens, num_subwords, hidden_size]
+    token_reprs = expanded_encoded_layers[batch_index, token_index, token_subword_index]
+    subword_pad_mask = token_subword_index.eq(0).unsqueeze(3).expand(
+        batch_size, num_tokens, num_subwords, hidden_size)
+    token_reprs.masked_fill_(subword_pad_mask, 0)
+    # [batch_size, num_tokens, hidden_size]
+    sum_token_reprs = torch.sum(token_reprs, dim=2)
+    # [batch_size, num_tokens]
+    num_valid_subwords = token_subword_index.ne(0).sum(dim=2)
+    pad_mask = num_valid_subwords.eq(0).long()
+    # Add ones to arrays where there is no valid subword.
+    divisor = (num_valid_subwords + pad_mask).unsqueeze(2).type_as(sum_token_reprs)
+    # [batch_size, num_tokens, hidden_size]
+    avg_token_reprs = sum_token_reprs / divisor
+    return avg_token_reprs
+
+
+def max_pooling(encoded_layers: torch.FloatTensor,
+                token_subword_index: torch.LongTensor) -> torch.Tensor:
+    batch_size, num_tokens, num_subwords = token_subword_index.size()
+    batch_index = torch.arange(batch_size).view(-1, 1, 1).type_as(token_subword_index)
+    token_index = torch.arange(num_tokens).view(1, -1, 1).type_as(token_subword_index)
+    _, num_total_subwords, hidden_size = encoded_layers.size()
+    expanded_encoded_layers = encoded_layers.unsqueeze(1).expand(
+        batch_size, num_tokens, num_total_subwords, hidden_size)
+    # [batch_size, num_tokens, num_subwords, hidden_size]
+    token_reprs = expanded_encoded_layers[batch_index, token_index, token_subword_index]
+    subword_pad_mask = token_subword_index.eq(0).unsqueeze(3).expand(
+        batch_size, num_tokens, num_subwords, hidden_size)
+    token_reprs.masked_fill_(subword_pad_mask, -float('inf'))
+    # [batch_size, num_tokens, hidden_size]
+    max_token_reprs, _ = torch.max(token_reprs, dim=2)
+    # [batch_size, num_tokens]
+    num_valid_subwords = token_subword_index.ne(0).sum(dim=2)
+    pad_mask = num_valid_subwords.eq(0).unsqueeze(2).expand(
+        batch_size, num_tokens, hidden_size)
+    max_token_reprs.masked_fill(pad_mask, 0)
+    return max_token_reprs
+
