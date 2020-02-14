a64a39651fc2b344a78c50a6869991731df245e7
Sheng Zhang <zhangsheng.zero@gmail.com>
2020-02-14 11:49:10 -0500

    Update label smoothing.

--- miso/modules/label_smoothing.py
+++ miso/modules/label_smoothing.py
@@ -1,26 +1,40 @@
+from overrides import overrides
 import torch
+from allennlp.common.registrable import Registrable
 
 
-class LabelSmoothing(torch.nn.Module):
-    "Implement label smoothing."
+class LabelSmoothing(torch.nn.Module, Registrable):
+    """Implement label smoothing."""
 
-    def __init__(self, padding_idx=0, smoothing=0.0):
-        super(LabelSmoothing, self).__init__()
-        self.criterion = torch.nn.KLDivLoss(reduction='sum')
-        self.padding_idx = padding_idx
-        self.confidence = 1.0 - smoothing
-        self.smoothing = smoothing
+    def __init__(self,
+                 pad_index: int = 0,
+                 smoothing: float = 0.0) -> None:
+        super().__init__()
+        self._criterion = torch.nn.KLDivLoss(reduction="sum")
+        self._pad_index = pad_index
+        self._smoothing = smoothing
+        self._confidence = 1.0 - smoothing
 
-    def forward(self, x, target):
+    def reset_parameters(self,
+                         pad_index: int = 0,
+                         smoothing: float = 0.0) -> None:
+        self._pad_index = pad_index
+        self._smoothing = smoothing
+        self._confidence = 1.0 - smoothing
+
+    @overrides
+    def forward(self,
+                x: torch.Tensor,
+                target: torch.Tensor) -> torch.Tensor:
         """
         :param x: log-probs [num_instances, vocab_size]
         :param target: [num_instances]
         """
         vocab_size = x.size(1)
         true_dist = torch.zeros_like(x)
-        true_dist.fill_(self.smoothing / (vocab_size - 2))
+        true_dist.fill_(self.smoothing / (vocab_size - 2))  # Exclude pad and target.
         true_dist.scatter_(1, target.unsqueeze(1), self.confidence)
-        true_dist[:, self.padding_idx] = 0
-        mask = target.eq(self.padding_idx)
+        true_dist[:, self._pad_index] = 0
+        mask = target.eq(self._pad_index)
         true_dist.masked_fill_(mask.unsqueeze(1), 0.0)
-        return self.criterion(x, true_dist)
+        return self._criterion(x, true_dist)
