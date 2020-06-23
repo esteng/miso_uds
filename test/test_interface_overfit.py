3e023cf9a337fc16bdecbd2ac869018280a851cd
esteng <elias.stengel@gmail.com>
2020-06-23 11:42:19 -0400

    transformer unit test
    Former-commit-id: 3bfd2ee653c48ce919f329099147e578eade480f

--- test/test_interface_overfit.py
+++ test/test_interface_overfit.py
@@ -1,63 +1,15 @@
 import pytest
 import sys 
 import os 
-import copy
-import shutil
-import json 
-from collections import namedtuple
 
 test_path = os.path.dirname(os.path.abspath(__file__))
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, path) 
 
 from allennlp.commands.train import Train, train_model_from_file
-from allennlp.common.util import import_submodules
 
-TOL = 0.0001
+from utils import assert_successful_overfit, read_metrics, setup_checkpointing_and_args
 
-def assert_successful_overfit(metrics, keys_and_expected_values):
-    for k, ev in keys_and_expected_values.items():
-        assert(abs(metrics[k] - ev) < TOL) 
-
-def read_metrics(output_dir):
-    metrics_path = os.path.join(output_dir, "metrics.json") 
-    assert(os.path.exists(metrics_path))
-    with open(metrics_path) as f1:
-        metrics = json.load(f1) 
-    return metrics 
-
-TrainArgs = namedtuple("TrainArgs", ["serialization_dir", "param_path"])
-
-def setup_checkpointing_and_args(config_path, output_dir):        
-    assert(os.path.exists(config_path))
-    # construct args
-    test_args = TrainArgs(serialization_dir = output_dir,
-                                   param_path = config_path)
-
-    # import miso 
-
-    include_package = ["miso.data.dataset_readers",
-                      "miso.models",
-                      "miso.modules.seq2seq_encoders",
-                      "miso.training",
-                      "miso.metrics"]
-
-    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    print(f"path is {path}" )
-    sys.path.insert(0, path) 
-    miso_path = os.path.join(path, "miso") 
-    sys.path.insert(0, miso_path) 
-
-    for package_name in include_package:
-        import_submodules(package_name)
-
-    # delete output dir if exists 
-    if os.path.exists(output_dir):
-        shutil.rmtree(output_dir) 
-
-    return test_args
-
-    
 def test_decomp_overfit():
     config_path = os.path.join(test_path, "configs", "overfit_decomp_base.jsonnet") 
     output_dir = os.path.join(test_path, "checkpoints", "overfit_decomp_base.ckpt") 
