bc25e30619c808648bf90ca39a3de6013e531ff8
esteng <elias.stengel@gmail.com>
2020-08-06 10:23:14 -0400

    starting hotfix
    Former-commit-id: 46be9f38c6bcbe72a36c9b6bd55cd005ba490631

--- test/test_transformer_overfit.py
+++ test/test_transformer_overfit.py
@@ -10,31 +10,6 @@ from allennlp.commands.train import Train, train_model_from_file
 
 from utils import assert_successful_overfit, read_metrics, setup_checkpointing_and_args
 
-#def test_decomp_transformer_dot_product_overfit():
-#    config_path = os.path.join(test_path, "configs", "overfit_decomp_transformer_dot_attention.jsonnet") 
-#    output_dir = os.path.join(test_path, "checkpoints", "overfit_decomp_transformer_dot_attention.ckpt") 
-#
-#    test_args = setup_checkpointing_and_args(config_path, output_dir) 
-#    train_model_from_file(test_args.param_path,
-#                          test_args.serialization_dir)
-#
-#    metrics = read_metrics(output_dir) 
-#    assert_successful_overfit(metrics, {"validation_s_f1": 100.0, 
-#                                        "training_uas": 100.0,
-#                                         "training_las": 100.0}) 
-    
-def test_decomp_transformer_no_coverage_overfit():
-    config_path = os.path.join(test_path, "configs", "overfit_decomp_transformer_no_coverage.jsonnet") 
-    output_dir = os.path.join(test_path, "checkpoints", "overfit_decomp_transformer_no_coverage.ckpt") 
-
-    test_args = setup_checkpointing_and_args(config_path, output_dir) 
-    train_model_from_file(test_args.param_path,
-                          test_args.serialization_dir)
-
-    metrics = read_metrics(output_dir) 
-    assert_successful_overfit(metrics, {"validation_s_f1": 100.0, 
-                                        "training_uas": 100.0,
-                                         "training_las": 100.0}) 
 
 def test_decomp_transformer_overfit():
     config_path = os.path.join(test_path, "configs", "overfit_decomp_transformer.jsonnet") 
