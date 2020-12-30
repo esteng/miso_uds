2bba754b6e044f92cd14f874e50cba269db6f81a
Elias Stengel-Eskin <estenge2@jhu.edu>
2020-12-30 16:19:04 -0500

    tuning bert and multilingual eval fix
    Former-commit-id: f9577e20ab05a366d6a41fe24298682ec850a9d7

--- grid_scripts/decode_decomp_multiling.sh
+++ grid_scripts/decode_decomp_multiling.sh
@@ -14,6 +14,6 @@ ml cudnn/7.5.0_cuda10.0
 cd /home/hltcoe/estengel/miso_research
 echo "CHECKPOINT DIR: ${CHECKPOINT_DIR} test_DATA ${TEST_DATA}"
 
-./syntax_experiments/decomp_train.sh -a conllu_predict -d ${CHECKPOINT_DIR} -i ${TEST_DATA} #&> ${CHECKPOINT_DIR}/${TEST_DATA}.conllu.out
+./syntax_experiments/decomp_train.sh -a conllu_predict_multi -d ${CHECKPOINT_DIR} -i ${TEST_DATA} #&> ${CHECKPOINT_DIR}/${TEST_DATA}.conllu.out
 #./experiments/decomp_test.sh -a spr_eval
 
--- grid_scripts/eval_all.sh
+++ grid_scripts/eval_all.sh
@@ -6,14 +6,14 @@ for lang in af de fr fi hu gl; do
     echo "${lang}" 
     python miso/metrics/conllu.py \
         /exp/estengel/ud_data/all_data/dev/${lang}-universal.conllu \
-        /exp/estengel/miso_res/ud_models_vocab/transformer/${lang}/dev.conllu
+        /exp/estengel/miso_res/ud_models_transformer_fix/transformer_pretrained/${lang}_syntax/dev.conllu
     echo "${lang} ENCODER" 
     python miso/metrics/conllu.py \
         /exp/estengel/ud_data/all_data/dev/${lang}-universal.conllu \
-        /exp/estengel/miso_res/ud_models_vocab/transformer_pretrained/${lang}_encoder/dev.conllu
+        /exp/estengel/miso_res/ud_models_transformer_fix/transformer_pretrained/${lang}_encoder/dev.conllu
     
-    echo "${lang} INTERMEDIATE" 
-    python miso/metrics/conllu.py \
-        /exp/estengel/ud_data/all_data/dev/${lang}-universal.conllu \
-        /exp/estengel/miso_res/ud_models_vocab/transformer_pretrained/${lang}_intermediate/dev.conllu
+    #echo "${lang} INTERMEDIATE" 
+    #python miso/metrics/conllu.py \
+    #    /exp/estengel/ud_data/all_data/dev/${lang}-universal.conllu \
+    #    /exp/estengel/miso_res/ud_models_vocab/transformer_pretrained/${lang}_intermediate/dev.conllu
 done
